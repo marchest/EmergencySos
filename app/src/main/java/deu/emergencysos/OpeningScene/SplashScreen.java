676e2b563c6b6c55bddcd2eb61c70a14f893aea5
marchest <marchest21@gmail.com>
2016-10-12 23:54:37 +0300

    Splash Screen tamam

---
+++ app/src/main/java/deu/emergencysos/OpeningScene/SplashScreen.java
@@ -0,0 +1,35 @@
+package deu.emergencysos.OpeningScene;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import deu.emergencysos.MainActivity;
+import deu.emergencysos.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                try {
+                    this.finish();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+            public void finish(){
+            // TODO
+            }
+
+        },2000);
+    }
+}
